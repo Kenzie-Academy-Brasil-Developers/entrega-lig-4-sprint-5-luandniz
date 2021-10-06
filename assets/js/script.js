0342adb42a4cd8f9b1fdf2c2d9ca852f5e9973d7
Luan <luandiniz91@hotmail.com>
2021-10-06 14:46:51 -0300

    winner-Diagonal

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,18 @@ const map = [
 ]
 
 const $wrapper = document.querySelector('#wrapper');
-
-
 for(let i=0; i<map.length; i++){
     //criando as colunas 
     let col = document.createElement('section');
-
     //identificação 
     col.classList = `column column_${i+1}`;
-
     //ppendurando as colunas no html
     $wrapper.appendChild(col);
-
     for(let j=0; j<map[i].length; j++){
         //criando as divs
         let div = document.createElement('div');
-
         //identificação 
         div.classList= `column${i+1}`;
-
         //pendurando as div nas seção
         col.appendChild(div);
     }
@@ -35,7 +28,8 @@ for(let i=0; i<map.length; i++){
 
 let usuario = true;
 let win = 0; //variavel para as condições de vitória
-let arr=[]; //array para as condições de vitória
+let arr = []; //array para as condições de vitória
+
 const $column = document.querySelectorAll('.column');
 $column.forEach(collumn => {
     collumn.addEventListener('click', (ev)=>{
@@ -44,7 +38,6 @@ $column.forEach(collumn => {
             arr.push(document.querySelector(`.column_${x}`).children);
         }
         let teste = arr[0][0].children.classList;//salvar o nome da class pra fazer a validação a partir dela
-        console.log(teste);
         //adicionar os discos
         let append = ev.currentTarget.children;//cria um array com os filhos
         let index = 0;
@@ -67,7 +60,6 @@ $column.forEach(collumn => {
         }else{
             const disc = document.createElement('div');
             disc.classList = 'discB';
-
             //coluna adicina um disco no ultimo filho que estiver vazio
             while(valid === 0 && append[5].hasChildNodes() === false){
                 if(append[index].hasChildNodes() === false){
@@ -75,13 +67,18 @@ $column.forEach(collumn => {
                     usuario = true;
                     valid++;
                 }else{
-                    index++;
-                }
-            }
-        }
-
-        
+                    index++;                   
+                }               
+            }           
+        }          
     });
 });
+//  
 
-    
+
+
+
+
+
+
+
